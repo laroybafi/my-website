175d871f4f0961e14c53ec42182755a238b143cd
Laroybafi Ahmad <38497798+laroybafi@users.noreply.github.com>
2022-02-01 01:10:24 +0700

    update

--- components/projects/RecentProject.js
+++ components/projects/RecentProject.js
@@ -10,54 +10,7 @@ export default function RecentProjects() {
       <p>Check out a few of my most recent publishings.</p>
       <Grid.Container gap={2}>
         <Grid>
-          <Card hoverable clickable  css={{ w: "330px" }} color="primary">
-            <Text h4 color="white">
-              Next UI
-            </Text>
-            <Text color="white">🚀 Beautiful and modern React UI library.</Text>
-            <Card.Footer>
-              <Link
-                target="_blank"
-                css={{ color: "white" }}
-                href="https://github.com/nextui-org/nextui"
-              >
-               <Button bordered color="gradient" auto>  Visit source code on GitHub. </Button>
-              </Link>
-            </Card.Footer>
-          </Card>
-        </Grid>
-        <Grid>
-          <Card hoverable clickable  css={{ w: "330px" }}>
-            <Text h4>Next UI</Text>
-            <Text>🚀 Beautiful and modern React UI library.</Text>
-            <Card.Footer>
-              <Link
-                color="primary"
-                target="_blank"
-                href="https://github.com/nextui-org/nextui"
-              >
-               <Button color="gradient">  Visit source code on GitHub. </Button>
-              </Link>
-            </Card.Footer>
-          </Card>
-        </Grid>
-        <Grid>
-          <Card hoverable clickable  css={{ w: "330px" }}>
-            <Text h4>Next UI</Text>
-            <Text>🚀 Beautiful and modern React UI library.</Text>
-            <Card.Footer>
-              <Link
-                color="primary"
-                target="_blank"
-                href="https://github.com/nextui-org/nextui"
-              >
-               <Button color="gradient">  Visit source code on GitHub. </Button>
-              </Link>
-            </Card.Footer>
-          </Card>
-        </Grid>
-        <Grid>
-          <Card hoverable clickable  css={{ w: "330px" }}>
+          <Card hoverable>
             <Text h4>Next UI</Text>
             <Text>🚀 Beautiful and modern React UI library.</Text>
             <Card.Footer>
